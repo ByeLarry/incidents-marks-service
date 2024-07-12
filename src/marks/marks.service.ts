65dbb424ac63ed17421aedc3894e1ed67edc8423
Solovey Roman <gasoss221510@gmail.com>
2024-07-12 09:32:29 +0300

    chore

--- src/marks/marks.service.ts
+++ src/marks/marks.service.ts
@@ -68,6 +68,7 @@ export class MarksService {
       });
       return {
         ...mark,
+        categoryId: mark.category.id,
         verified,
         isMyVerify: !!userVerification,
         distance: distance[0].distance,
