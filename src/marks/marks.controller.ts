deed77bcb5acd63ec7d1b3e8f6d759ece70b4e73
Solovey Roman <gasoss221510@gmail.com>
2024-09-02 18:08:24 +0300

    fix: rabbitmq connection in docker

--- src/marks/marks.controller.ts
+++ src/marks/marks.controller.ts
@@ -1,8 +1,7 @@
-import { MessageBody } from '@nestjs/websockets';
 import { MarksService } from './marks.service';
 import { MsgMarksEnum } from 'src/utils/msg.marks.enum';
 import { CoordsDto } from './dto/coords.dto';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Controller } from '@nestjs/common';
 import { VerifyMarkDto } from './dto/verify-mark.dto';
 import { MarkDto } from './dto/mark.dto';
@@ -12,37 +11,37 @@ import { CreateMarkDto } from './dto/create-mark.dto';
 export class MarksController {
   constructor(private readonly marksService: MarksService) {}
 
-  @MessagePattern({ cmd: MsgMarksEnum.TEST_SEND })
-  test(@MessageBody() data: string) {
+  @MessagePattern(MsgMarksEnum.TEST_SEND)
+  test(@Payload() data: string) {
     return this.marksService.test(data);
   }
 
-  @MessagePattern({ cmd: MsgMarksEnum.MAP_INIT_SEND })
-  getMarks(@MessageBody() data: CoordsDto) {
+  @MessagePattern(MsgMarksEnum.MAP_INIT_SEND)
+  getMarks(@Payload() data: CoordsDto) {
     return this.marksService.getMarks(data);
   }
 
-  @MessagePattern({ cmd: MsgMarksEnum.MARK_GET_SEND })
-  getMark(@MessageBody() data: MarkDto) {
+  @MessagePattern(MsgMarksEnum.MARK_GET_SEND)
+  getMark(@Payload() data: MarkDto) {
     return this.marksService.getMark(data);
   }
 
-  @MessagePattern({ cmd: MsgMarksEnum.MARK_VERIFY_TRUE_SEND })
-  verifyTrue(@MessageBody() data: VerifyMarkDto) {
+  @MessagePattern(MsgMarksEnum.MARK_VERIFY_TRUE_SEND)
+  verifyTrue(@Payload() data: VerifyMarkDto) {
     return this.marksService.verifyTrue(data);
   }
 
-  @MessagePattern({ cmd: MsgMarksEnum.MARK_VERIFY_FALSE_SEND })
-  verifyFalse(@MessageBody() data: VerifyMarkDto) {
+  @MessagePattern(MsgMarksEnum.MARK_VERIFY_FALSE_SEND)
+  verifyFalse(@Payload() data: VerifyMarkDto) {
     return this.marksService.verifyFalse(data);
   }
 
-  @MessagePattern({ cmd: MsgMarksEnum.CATEGORIES_SEND })
+  @MessagePattern(MsgMarksEnum.CATEGORIES_SEND)
   getCategories() {
     return this.marksService.getCategories();
   }
 
-  @MessagePattern({ cmd: MsgMarksEnum.CREATE_MARK_SEND })
+  @MessagePattern(MsgMarksEnum.CREATE_MARK_SEND)
   createMark(data: CreateMarkDto) {
     return this.marksService.createMark(data);
   }
