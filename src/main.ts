a03014316e72bee4ca77febb526e958ec969259b
Solovey Roman <gasoss221510@gmail.com>
2024-09-02 15:55:12 +0300

    feature: add rabbit mq instead of tcp connection

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { LoggingInterceptor } from './interceptors/logger.interceptor';
+import { MARKS_RMQ_QUEUE } from './utils/consts';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.RMQ,
       options: {
-        host: `${process.env.HOST}`,
-        port: Number(process.env.PORT),
+        urls: [`${process.env.RMQ_HOST}`],
+        queue: MARKS_RMQ_QUEUE,
+        queueOptions: {
+          durable: false,
+        },
       },
     },
   );
---
+++ src/utils/consts.ts
@@ -0,0 +1,2 @@
+export const MARKS_RMQ_QUEUE = 'marks_queue';
+export const AUTH_RMQ_QUEUE = 'auth_queue';
