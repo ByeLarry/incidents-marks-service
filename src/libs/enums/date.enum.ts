64eef07df5f3736dd99862088a77b03a7a0a301f
Solovey Roman <gasoss221510@gmail.com>
2024-09-30 11:03:38 +0300

    chore: update date enum

--- src/libs/enums/date.enum.ts
+++ src/libs/enums/date.enum.ts
@@ -1,4 +1,9 @@
 export enum DateEnum {
+  THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000,
   THREE_DAYS = 3 * 24 * 60 * 60 * 1000,
   ONE_DAY = 1 * 24 * 60 * 60 * 1000,
+  ONE_HOUR = 60 * 60 * 1000,
+  ONE_MINUTE = 60 * 1000,
+  TEN_MINUTES = 10 * 60 * 1000,
+  ONE_SECOND = 1000,
 }
